fff0fd6a00ae5a83dfc0770a28f3ed13aeeb3184
Scott Gauthreaux <me@scottgauthreaux.com>
2016-11-09 11:46:38 -0800

    Fixed cpp file, include was fucked up

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -3,14 +3,7 @@
  *
  * The implementation of the scoreboard.h interface
  */
-#include"scoreboard.h"
 
-void Scoreboard::clearScoreboard() {
-   for(unsigned int i = 0; i < scores.size(); ++i) {
-      for(unsigned int j = 0; j < scores[i].size(); ++j) {
-         scores[i][j] = 0;
-      }
-   }
 
 #include "scoreboard.h"
 
@@ -84,3 +77,12 @@ int getTotalScore(int competitor) {
 
   return totalScore;
 }
+
+
+
+void Scoreboard::clearScoreboard() {
+   for(unsigned int i = 0; i < scores.size(); ++i) {
+      for(unsigned int j = 0; j < scores[i].size(); ++j) {
+         scores[i][j] = 0;
+      }
+   }
