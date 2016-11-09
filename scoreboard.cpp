b34adf699e7f5f48032fb67ea9e5e5c19aba633f
Forrest Taylor <ftaylor@mail.csuchico.edu>
2016-11-09 11:32:08 -0800

    Update scoreboard.cpp

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -3,3 +3,10 @@
  *
  * The implementation of the scoreboard.h interface
  */
+#include "Scoreboard.h"
+
+//Taylor, Forrest
+int Scoreboard::getScore(int competitor, int period, int score)
+{
+   
+}
