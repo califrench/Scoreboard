edfbac865eb8d5487c911af9a84e644bfc216bc2
ZaName <ivyhillcaleb@gmail.com>
2016-11-09 11:34:07 -0800

    Added getTotalScore function

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -3,3 +3,17 @@
  *
  * The implementation of the scoreboard.h interface
  */
+
+int getTotalScore(int competitor) {
+  int totalScore = 0;
+
+  if (competitor < 1 || competitor > scores.size())
+    return 0; //out of range
+
+  for (int i = 1; i <= scores[competitor].size(); ++i)
+  {
+    totalScore += getScore(competitor, i);
+  }
+
+  return totalScore;
+}
